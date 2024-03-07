3cd4fbd8b13e203637d9fb144f21d440d3ac18cd
OscarMC2003 <omarcosconde@gmail.com>
2024-03-07 14:04:51 +0100

    7/3
    ya funciona el login arreglado por ander

---
+++ foros-utad/app/actividades/page.js
@@ -0,0 +1,9 @@
+import Actividades from "@/components/actividades/Actividades";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Actividades />
+    </main>
+  );
+}
