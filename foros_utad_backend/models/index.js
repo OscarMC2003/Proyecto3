70cac549f502018c043a4616d293ea19023dae59
andrés pérez <117923181+andresperezvlc@users.noreply.github.com>
2024-03-13 17:59:49 +0100

    foro

--- foros_utad_backend/models/index.js
+++ foros_utad_backend/models/index.js
@@ -2,6 +2,7 @@ const models = {
     usersModel: require('./nosql/users'),
     clasesModel: require('./nosql/clases'),
     actividadesModel: require('./nosql/actividades'),
+    forosModel: require('./nosql/foro'),
     storageModel: require('./nosql/storage')
 }
 
