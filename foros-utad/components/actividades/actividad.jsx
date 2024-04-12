27c23a156ec9a5fa47cf32f6024db4b3b29dc00b
andrés pérez <117923181+andresperezvlc@users.noreply.github.com>
2024-04-12 09:32:47 +0200

    chat actividades temporal

--- foros-utad/components/actividades/actividad.jsx
+++ foros-utad/components/actividades/actividad.jsx
@@ -8,6 +8,37 @@ const Actividad = ({ handleClose, show, id }) => {
   const showHideClassName = show ? "fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 overflow-y-auto" : "hidden";
   const animationClassName = show ? "animated-fadeIn" : "animated-fadeOut";
 
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const getMensajes = async () => {
+      try {
+        const respuesta = await fetch(`http://localhost:9000/api/foro/65f1dab639b3ca6de3a667ad/mensajes`);
+        if (respuesta.ok) {
+          const datos = await respuesta.json();
+          const mensajes = datos.map(mensaje => mensaje.texto);
+          // Mostrar mensajes consola a modo comprobación
+          console.log(mensajes)
+          setMessages(mensajes);
+        } else {
+          console.error("Error obteniendo los mensajes");
+        }
+      } catch (error) {
+        console.error("Error obteniendo mensajes del foro", error);
+      }
+    };
+
+    const intervalo = setInterval(() => {
+      if (show) {
+        getMensajes();
+      }
+    }, 1000);
+
+    if (show) {
+      getMensajes();
+    }
+
+    return () => clearInterval(intervalo)
+  }, [id, show]);
   const handleCloseOutside = (event) => {
     if (event.target === event.currentTarget) {
       handleClose();
@@ -17,8 +48,8 @@ const Actividad = ({ handleClose, show, id }) => {
   useEffect(() => {
     // if (!id) return; // Manejar el caso donde id es null o undefined
     if (user) { // cuando no esta logeado, user es null
-    	console.log(user._id);
-    	// parametros disponibles: _id, email, iat (fecha de login), exp (fecha de expiracion)
+      console.log(user._id);
+      // parametros disponibles: _id, email, iat (fecha de login), exp (fecha de expiracion)
     }
 
     const fetchData = async () => {
@@ -36,7 +67,7 @@ const Actividad = ({ handleClose, show, id }) => {
 
   return (
     <div className={`${showHideClassName} ${animationClassName}`} onClick={handleCloseOutside}>
-      <div className="bg-opacity-25 p-8 rounded shadow-md max-w-screen-lg w-full my-8 mx-auto" style={{marginTop: '20vh'}}>
+      <div className="bg-opacity-25 p-8 rounded shadow-md max-w-screen-lg w-full my-8 mx-auto" style={{ marginTop: '20vh' }}>
         <header className="flex justify-between items-center mb-4">
           <img src="/images/cuadrado.png" alt="Imagen Izquierda" style={{ width: '75px', height: 'auto' }} />
         </header>
@@ -47,13 +78,28 @@ const Actividad = ({ handleClose, show, id }) => {
                 <img src="/images/cuadrado.png" alt="Actividad" className="w-full object-cover" style={{ height: '250px' }} />
                 <div className="bg-white p-6 flex flex-col md:flex-row">
                   <div className="flex-grow w-2/3">
-                    <h1 className="text-2xl font-bold mb-4 montBlack">{actividad.asunto}</h1>                        
+                    <h1 className="text-2xl font-bold mb-4 montBlack">{actividad.asunto}</h1>
                     <p className="text-gray-700 max-w-2/3" style={{ wordWrap: 'break-word' }}>{actividad.objetivo}</p> {/* Modificación aquí */}
                     <div className="mt-4 border-t pt-4">
                       <h2 className="font-semibold mb-2">Documentos adjuntos:</h2>
                       <div className="flex flex-wrap gap-4">
                         <img src="/images/cuadrado.png" alt="Actividad" className="w-auto"></img>
                         <img src="/images/cuadrado.png" alt="Actividad" className="w-auto"></img>
+                      </div>
+                      <div className="flex flex-col mb-5">
+                        <span className="font-bold">Comentarios:</span>
+                        <button className="mt-2 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">
+                          Agregar comentario
+                        </button>
+                        <div className="flex flex-col overflow-hidden mt-4" style={{ maxHeight: '30rem' }}>
+                          {messages.slice(-4).map((message, index) => (
+                            <div key={index} className="p-2">
+                              <div className="bg-gray-200 p-2 rounded-lg inline-block">
+                                <span className="backend-message">{message}</span>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -62,11 +108,9 @@ const Actividad = ({ handleClose, show, id }) => {
                       <button aria-label="Calendario" className="p-2 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400">
                         <img src="/images/Calendario.png" alt="Calendario" />
                       </button>
-
                       <button aria-label="Compartir" className="p-2 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400">
                         <img src="/images/Compartir.png" alt="Compartir" />
                       </button>
-
                       <button aria-label="Bookmark" className="p-2 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400">
                         <img src="/images/Guardar.png" alt="Bookmark" />
                       </button>
@@ -106,6 +150,8 @@ const Actividad = ({ handleClose, show, id }) => {
       </div>
     </div>
   );
+  
+  
 };
 
 export default Actividad;
