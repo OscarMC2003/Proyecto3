581a8ba0ccc875d419f70e925c5bbe72186cd44a
OscarMC2003 <omarcosconde@gmail.com>
2024-05-09 20:12:08 +0200

    Pagina de foros como individual
    tiene que modificarse el aspecto pero lo tecnico esta todo

--- foros-utad/app/foroInterior/page.js
+++ foros-utad/app/foroInterior/page.js
@@ -1,9 +1,14 @@
+"use client"
 import ForoInterior from "@/components/foro/foroInterior";
+import { useSearchParams } from "next/navigation"
 
 export default function Home() {
+  const valor = useSearchParams();
+  const identificadorForo = valor.get('foroId')
+  const identificadorUser = valor.get('idUserIniciado')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <ForoInterior />
+      <ForoInterior IDs={{identificadorUser, identificadorForo}}/>
     </main>
   );
 }
--- foros-utad/components/lista/lista.jsx
+++ foros-utad/components/lista/lista.jsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import ForoInterior from '../foro/foroInterior';
+import { useRouter } from 'next/navigation'
 
 function RectangleList({ foros, idUserIniciado }) { // Se agrega destructuración para recibir el prop correctamente
 
-  const [showPopup, setShowPopup] = useState(null); // Se inicializa con null
-  const togglePopup = (foroId) => {
+  //const [showPopup, setShowPopup] = useState(null); // Se inicializa con null
+  /*const togglePopup = (foroId) => {
     setShowPopup(foroId === showPopup ? null : foroId);
-  };
+  };*/
+
+  const router = useRouter()
+
+  const handleClick = (foroId) => {
+    alert(foroId)
+    router.push(`/foroInterior?foroId=${foroId}&idUserIniciado=${idUserIniciado}`);
+  }
+
 
   return (
     <div style={{ width: '100%' }}>
@@ -18,11 +27,11 @@ function RectangleList({ foros, idUserIniciado }) { // Se agrega destructuració
           borderRadius: '8px',
           width: '100%', // Hacer que cada caja ocupe todo el ancho disponible
         }}>
-          <button style={{ width: '100%' }} onClick={() => togglePopup(foro._id)}>
+          <button style={{ width: '100%' }} onClick={() =>handleClick(foro._id) }>
             <h2>{foro.name}</h2>
             <p>{foro.descripcion}</p>
           </button>
-          {showPopup === foro._id && <ForoInterior idUserIniciado={idUserIniciado} handleClose={() => togglePopup(foro._id)} show={showPopup} id={foro._id} />}
+          {/* {showPopup === foro._id && <ForoInterior idUserIniciado={idUserIniciado} handleClose={() => togglePopup(foro._id)} show={showPopup} id={foro._id} />} */}
         </div>
       ))}
     </div>
