356f2f1f784d0da44adb1bbaac630a7b9c0b600d
OscarMC2003 <omarcosconde@gmail.com>
2024-05-30 12:21:50 +0200

    fotos
    fotos

--- foros-utad/utils/fotos.js
+++ foros-utad/utils/fotos.js
@@ -4,7 +4,14 @@ const fotos = [
     "/images/actividades/base3.jpeg",
     "/images/actividades/base4.jpeg",
     "/images/actividades/base5.jpeg",
-
+    "/images/actividades/base6.jpeg",
+    "/images/actividades/base7.jpeg",
+    "/images/actividades/base8.jpeg",
+    "/images/actividades/base9.jpeg",
+    "/images/actividades/base10.jpeg",
+    "/images/actividades/base11.jpeg",
+    "/images/actividades/base12.jpeg",
+    "/images/actividades/base13.jpeg"
   ];
 
 export default fotos;
